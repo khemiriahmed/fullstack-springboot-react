b7d0fa4aa7b840a0b094f2a551bbea3f704fc13b
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-17 15:17:38 +0100

    @putMapping edit user

--- backend/src/main/java/com/akh/fullstack/controller/UserController.java
+++ backend/src/main/java/com/akh/fullstack/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,27 +15,38 @@ import com.akh.fullstack.exception.UserNotFoundException;
 import com.akh.fullstack.model.User;
 import com.akh.fullstack.repository.UserRepository;
 
-
 @RestController
 @CrossOrigin("http://localhost:3000/")
 public class UserController {
- @Autowired 
- private UserRepository userRepository;
- 
- @PostMapping("user")
- User newUser(@RequestBody User newUser){
-	 return userRepository.save(newUser);
- }
- 
- @GetMapping("users")
- List<User> getAllUsers(){
-	 return userRepository.findAll();
- }
- @GetMapping("user/{id}")
- 
-  User getUserById(@PathVariable Long id) {
-	 return userRepository.findById(id)
-			 .orElseThrow(()->new UserNotFoundException(id));
- }
- 
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostMapping("user")
+	User newUser(@RequestBody User newUser) {
+		return userRepository.save(newUser);
+	}
+
+	@GetMapping("users")
+	List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@GetMapping("user/{id}")
+
+	User getUserById(@PathVariable Long id) {
+		return userRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException(id));
+	}
+
+	@PutMapping("/user/{id}")
+	User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+		return userRepository.findById(id)
+				.map(user -> {
+					user.setUsername(newUser.getUsername());
+					user.setName(newUser.getName());
+					user.setEmail(newUser.getEmail());
+					return userRepository.save(user);
+				}).orElseThrow(() -> new UserNotFoundException(id));
+	}
+
 }
