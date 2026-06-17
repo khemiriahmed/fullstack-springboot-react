c8bb78d8eb9a723c85dde4701e0717f25a403653
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-17 15:51:11 +0100

    Add link edit & delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './layout/Navbar';
-import Home from './pages/Home';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
 import EditUser from "./users/EditUser";
@@ -10,15 +9,15 @@ import ViewUser from "./users/ViewUser";
 function App() {
   return (
     <div className="App">
-         <Router>
-     <Navbar/>
-      <Routes>
-     <Route exact path="/" element={<Home />} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/adduser" element={<AddUser />} />
-          {/* <Route exact path="/edituser/:id" element={<EditUser />} />
-          <Route exact path="/viewuser/:id" element={<ViewUser />} /> */}
-</Routes>
-          </Router>
+          { <Route exact path="/edituser/:id" element={<EditUser />} />
+          /*<Route exact path="/viewuser/:id" element={<ViewUser />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
