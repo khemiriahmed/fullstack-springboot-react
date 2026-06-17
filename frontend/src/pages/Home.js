dc1fc7c57fb2062fdec3f85bfac917711f2a2965
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-17 14:35:31 +0100

    List users

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function Home() {
+
+  const [users, setUsers] = useState([])
+  const { id } = useParams();
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get('http://localhost:8080/users');
+    setUsers(result.data)
+  }
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">#id</th>
+              <th scope="col">#name</th>
+              <th scope="col">username</th>
+              <th scope="col">email</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user, index) => (
+                <tr key={user.id ?? index}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                     <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewuser/${user.id}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    // onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
